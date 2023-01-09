a3abf22b372e0cf62bf6f9f0dbfe905fba66f967
chinmayirage <chinmayirage37@gmail.com>
2023-01-09 20:56:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cdnmethod(request):
+    return render(request,'cdnmethod.html')
+
+def download(request):
+    return render(request,'download.html')
